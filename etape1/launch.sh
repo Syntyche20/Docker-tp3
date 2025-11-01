65da46ee27c92264418ae3a05fd4ebab34f565af
Syntyche <lois.mangaboula@gmail.com>
2025-11-01 18:45:01 +0100

    Correction des launch.sh pour Windows et volumes bind mount

--- etape1/launch.sh
+++ etape1/launch.sh
@@ -1,16 +1,20 @@
-# Supprimer les containers existants
-docker rm -f script http 2>$null
+# Supprimer tous les containers existants
+docker rm -f $(docker ps -aq) 2>$null
 
 # Créer le réseau Docker s'il n'existe pas
 docker network inspect tp3-network 2>$null || docker network create tp3-network
 
+# Définir les chemins absolus
+$srcPath = "$PWD\src"
+$configPath = "$PWD\config\default.conf"
+
 # Lancer le container PHP-FPM
-docker run -d --name script --network tp3-network -v ./src:/app php:8.2-fpm
+docker run -d --name script --network tp3-network -v "$srcPath:/app" php:8.2-fpm
 
 # Lancer le container Nginx
 docker run -d --name http --network tp3-network -p 8080:80 `
-  -v ./src:/app `
-  -v ./config/default.conf:/etc/nginx/conf.d/default.conf `
+  -v "$srcPath:/app" `
+  -v "$configPath:/etc/nginx/conf.d/default.conf" `
   nginx:latest
 
 Write-Host "Containers lancés. Ouvrir http://localhost:8080/ pour vérifier phpinfo()"
--- etape2/launch.sh
+++ etape2/launch.sh
@@ -1,29 +1,33 @@
-# Supprimer anciens containers
-docker rm -f data 2>$null
-docker rm -f script 2>$null
-docker rm -f http 2>$null
+# Supprimer tous les containers existants
+docker rm -f $(docker ps -aq) 2>$null
 
 # Créer le réseau s'il n'existe pas
 docker network inspect tp3-network 2>$null || docker network create tp3-network
 
+# Définir les chemins absolus
+$srcPath = "$PWD\src"
+$configPath = "$PWD\config\default.conf"
+$dbPath = "$PWD\db"
+$phpPath = "$PWD\php"
+
 # Lancer MariaDB
 docker run -d --name data --network tp3-network `
   -e MARIADB_RANDOM_ROOT_PASSWORD=yes `
-  -v ./db:/docker-entrypoint-initdb.d `
+  -v "$dbPath:/docker-entrypoint-initdb.d" `
   mariadb:latest
 
-# Construire PHP avec mysqli
-docker build -t php-mysqli ./php
+# Construire l'image PHP avec mysqli
+docker build -t php-mysqli "$phpPath"
 
 # Lancer PHP-FPM
 docker run -d --name script --network tp3-network `
-  -v ./src:/app `
+  -v "$srcPath:/app" `
   php-mysqli
 
 # Lancer Nginx
 docker run -d --name http --network tp3-network -p 8080:80 `
-  -v ./src:/app `
-  -v ./config/default.conf:/etc/nginx/conf.d/default.conf `
+  -v "$srcPath:/app" `
+  -v "$configPath:/etc/nginx/conf.d/default.conf" `
   nginx:latest
 
 Write-Host "Etape 2 lancée. Vérifier http://localhost:8080/ et http://localhost:8080/test.php"
